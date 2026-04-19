dd01e1a81115dda4e80f5b33e68982035936181e
bruce-gray <brucefledge@gmail.com>
2026-04-19 16:29:31 +0100

    added merge sort and more methods for managing tickets

--- queue_manager.py
+++ queue_manager.py
@@ -3,6 +3,35 @@ import task
 import persistence
 import validation
 
+def merge_sort(lst): # sorts a list of tickets by deadline
+    # base case - list of 1 or 0 elements doesn't need to be sorted
+    if len(lst) <= 1:
+        return lst
+    
+    # split the list in half
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+
+    # merge the sorted halves
+    return merge(left,right)
+
+def merge(left,right): # merges two halves by comparing priority and thenm deadlines, appends the earlier deadline first
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if (left[i].priority, left[i].deadline) < (right[j].priority, right[j].deadline):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    # append any remaining elements from either half
+    result += left[i:]
+    result += right[j:]
+    return result
+
 class QueueManager:
     def __init__(self):
         self.tickets = persistence.load_tickets() # flat list for linear search
@@ -42,3 +71,39 @@ class QueueManager:
         self.tickets.append(new_ticket)
         heapq.heappush(self.heap, (new_ticket.priority, new_ticket.deadline, new_ticket.ticket_id, new_ticket))
         persistence.save_tickets(self.tickets)
+
+    def assign_ticket(self, ticket_id, assignee):
+        for i in range(len(self.tickets)):
+            if self.tickets[i].ticket_id == ticket_id:
+                self.tickets[i].assignee = assignee
+                self.tickets[i].status = 'In Progress'
+                self.rebuild_heap()
+                persistence.save_tickets(self.tickets)
+                break
+        else:
+            raise ValueError(f'Ticket with ID {ticket_id} not found.')
+        
+    def close_ticket(self, ticket_id, status):
+        if status not in ['Completed', 'Closed']:
+            raise ValueError('Status must be Completed or Closed.')
+        for i in range(len(self.tickets)):
+            if self.tickets[i].ticket_id == ticket_id:
+                self.tickets[i].status = status
+                persistence.archive_ticket(self.tickets[i])
+                self.tickets.remove(self.tickets[i])
+                self.rebuild_heap()
+                persistence.save_tickets(self.tickets)
+                break
+        else:
+            raise ValueError(f'Ticket with ID {ticket_id} not found.')
+        
+    def get_tickets(self):
+        tickets = [item[3] for item in sorted(self.heap)]
+        return merge_sort(tickets)
+    
+    def get_ticket(self, ticket_id):
+        for i in range(len(self.tickets)):
+            if self.tickets[i].ticket_id == ticket_id:
+                return(self.tickets[i])
+        else:
+            raise ValueError(f'Ticket with ID {ticket_id} not found.')
